67fbff291a8909de8bf8c1dee34fb8937c2217d5
zorg <Oleksandr.Kryvenkyi@deltatre.com>
2026-03-31 11:01:01 +0100

    fix domain warning

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
+  allowedDevOrigins: ["rezka.zorg.local", "*.zorg.local", "rezka.local", "*.local"],
 };
 
 export default nextConfig;
